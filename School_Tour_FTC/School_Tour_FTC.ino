1c5733a362ddd30cd4e409b9660c06fe0ade5724
Pham Gia Phu <giaphu899@gmail.com>
2026-01-18 22:56:57 +0700

    Added School_Tour_FTC.ino: - Arm and Claw subsystems - simple 2-motor control

---
+++ School_Tour_FTC/School_Tour_FTC.ino
@@ -0,0 +1,181 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <PS2X_lib.h>
+
+// #########
+// CONFIGURE
+// #########
+
+/* CONSTANTS */
+// Drive motor speed
+#define SPD_DRIVE           3072
+#define SPD_DEAD            80
+// Servo PW
+#define PW_MIN              440
+#define PW_MAX              2270
+
+/* PWM channels */
+// DC Motor
+#define MOTOR_DRIVE_LEFT_A  8 
+#define MOTOR_DRIVE_LEFT_B  9
+#define MOTOR_DRIVE_RIGHT_A 14
+#define MOTOR_DRIVE_RIGHT_B 15
+// Servo
+#define SERVO_ARM_X         2
+#define SERVO_ARM_Y         3
+#define SERVO_CLAW          4
+
+/* PS2 pins */
+#define PS2_DAT             12
+#define PS2_CMD             13
+#define PS2_ATT             15
+#define PS2_CLK             14
+
+
+// ##########
+// PWM DRIVER
+// ##########
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+void PWMDriver_init() {
+  Serial.print(F("Initializing PWM controller..."));
+
+  // PWM Init
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(50);
+  // Wire Init
+  Wire.setClock(400000);
+
+  Serial.println(F("done."));
+}
+
+
+// ############
+// HARDWARE API
+// ############
+
+/* PS2 Controller */
+PS2X ps2;
+
+void PS2_init() {
+  Serial.print(F("Initializing PS2 controller..."));
+
+  uint8_t error = ps2.config_gamepad(PS2_CLK, PS2_CMD, PS2_ATT, PS2_DAT);
+  while (error != 0) {
+    switch (error) {
+      case 1:
+        Serial.println("Error code 1: No controller found, check wiring.");
+        break;
+      case 2:
+        Serial.println("Error code 2: Controller found but not accepting commands.");
+        break;
+      case 3:
+        Serial.println("Error code 3: Controller refusing to enter Pressures mode, may not support it. ");
+        break;
+    }
+
+    error = ps2.config_gamepad(PS2_CLK, PS2_CMD, PS2_ATT, PS2_DAT);
+  }
+
+  Serial.println(F("done."));
+}
+
+/* Control */
+void dc_control(uint8_t channelA, uint8_t channelB, int16_t speed, bool brake = false) {
+  if (speed > -SPD_DEAD && speed < SPD_DEAD)
+    speed = 0;
+  uint16_t speed_a = (brake) ? 4095 : ((speed > 0) ? speed : 0);
+  uint16_t speed_b = (brake) ? 4095 : ((speed < 0) ? (-speed) : 0);
+
+  pwm.setPWM(channelA, 0, speed_a);
+  pwm.setPWM(channelB, 0, speed_b);
+}
+
+void servo_control(uint8_t channel, float value) {
+  pwm.writeMicroseconds(channel, PW_MIN + value * (PW_MAX - PW_MIN));
+}
+
+
+// ##########
+// SUBSYSTEMS
+// ##########
+
+void pos_update(float &pos, float value, float _min, float _max) {
+  if (value < 0) {
+    if (pos - value >= _min) pos -= value;
+  }
+  
+  if (value > 0) {
+    if (pos + value <= _max) pos += value;
+  }
+}
+
+/* DRIVETRAIN */
+void drivetrain_update() {
+  int16_t speed_l = map(ps2.Analog(PSS_LY), 0, 255, SPD_DRIVE, -SPD_DRIVE);
+  dc_control(MOTOR_DRIVE_LEFT_A, MOTOR_DRIVE_LEFT_B, speed_l);
+
+  int16_t speed_r = map(ps2.Analog(PSS_RY), 0, 255, -SPD_DRIVE, SPD_DRIVE);
+  dc_control(MOTOR_DRIVE_RIGHT_A, MOTOR_DRIVE_RIGHT_B, speed_r);
+}
+
+/* ARM */
+float arm_x_pos;
+float arm_y_pos;
+
+void arm_init() {
+  arm_x_pos = 0.0;
+  arm_y_pos = 0.0;
+}
+
+void arm_update() {
+  if (ps2.Button(PSB_TRIANGLE)) pos_update(arm_y_pos,  0.1, 0.0, 1.0);
+  if (ps2.Button(PSB_CROSS))    pos_update(arm_y_pos, -0.1, 0.0, 1.0);
+  if (ps2.Button(PSB_CIRCLE))   pos_update(arm_x_pos,  0.1, 0.0, 1.0);
+  if (ps2.Button(PSB_SQUARE))   pos_update(arm_x_pos, -0.1, 0.0, 1.0);
+
+  servo_control(SERVO_ARM_X, arm_x_pos);
+  servo_control(SERVO_ARM_Y, arm_y_pos);
+}
+
+/* CLAW */
+float claw_pos;
+
+void claw_init() {
+  claw_pos = 0.0;
+}
+
+void claw_update() {
+  if (ps2.Button(PSB_PAD_UP))   pos_update(claw_pos,  0.1, 0.0, 1.0);
+  if (ps2.Button(PSB_PAD_DOWN)) pos_update(claw_pos, -0.1, 0.0, 1.0);
+
+  servo_control(SERVO_CLAW, claw_pos);
+}
+
+
+// #################
+// ARDUINO FUNCTIONS
+// #################
+
+void setup() {
+  Serial.begin(115200);  // ESP32 baud rate (bps)
+
+  PWMDriver_init();
+  PS2_init();
+
+  arm_init();
+  claw_init();
+}
+
+void loop() {
+  ps2.read_gamepad();  // update from controller
+
+  drivetrain_update();
+  
+  // ARM_X = SQUARE, ARM_Y = CROSS  
+  arm_update();
+  // CLAW = TRIANGLE
+  claw_update();
+}
